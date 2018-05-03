c8acc953958aa6c65d7e679f754c4b68b996d10a
Felix Borzik <borzik1992@gmail.com>
2018-05-03 15:04:24 +0300

    convert css to scss

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('build', function () {
+  return gulp.src('./src/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist'));
+});
